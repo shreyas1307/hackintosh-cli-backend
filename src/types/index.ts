1158481328f3c3aab04896dd15f6863644abade7
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2020-06-12 18:00:09 +0530

    Small formatting fix

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,12 @@
-export type GithubPackageLatest = { package: string, version: number | string, release_id: string | number }
-export type GithubPackageAllVersions = { package: string, version: { release_id: string | number, release_version: number | string }[] }
+export type GithubPackageLatest = {
+    package: string,
+    version: number | string,
+    release_id: string | number
+}
+export type GithubPackageAllVersions = {
+    package: string,
+    version: {
+        release_id: string | number,
+        release_version: number | string
+    }[]
+}
