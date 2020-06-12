6521db9f33c6f06d5b3ba5e3ab4f276a5476e850
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2020-06-12 14:50:36 +0530

    update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,2 @@
-export type GithubPackageLatest = { package: string, version: number }
-export type GithubPackageAllVersions = { package: string, version: number[] }
+export type GithubPackageLatest = { package: string, version: number | string, release_id: string | number }
+export type GithubPackageAllVersions = { package: string, version: { release_id: string | number, release_version: number | string }[] }
