393197ce9d33d9a79c2de8ebbdd2d2107dae3ea3
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2020-06-12 09:41:01 +0530

    middleware

---
+++ src/middleware/logger.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express'
+
+const loggerMiddleware = (req: Request, resp: Response, next: NextFunction) => {
+
+    console.log('Request logged:', req.method, req.path)
+    next()
+}
+
+export default loggerMiddleware
