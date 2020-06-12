489417ffd2111afb96f3357e828ed5ee15cfbd7f
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2020-06-12 14:50:29 +0530

    Added some dummy data for testing

---
+++ dummyData/index.ts
@@ -0,0 +1,735 @@
+import { GithubPackageLatest, GithubPackageAllVersions } from "../src/types"
+
+export const latest: GithubPackageLatest[] = [
+    {
+        package: "khronokernel/SmallTree-I211-AT-patch",
+        version: "1.3.0",
+        release_id: 25575621
+    },
+    {
+        package: "Mieze/AtherosE2200Ethernet",
+        version: "2.2.2",
+        release_id: 14061603
+    },
+    {
+        package: "Sniki/OS-X-USB-Inject-All",
+        version: "v0.7.5",
+        release_id: 24748286
+    },
+    {
+        package: "Mieze/RTL8111_driver_for_OS_X",
+        version: "v2.2.2",
+        release_id: 10009742
+    },
+    {
+        package: "acidanthera/BrcmPatchRAM",
+        version: "2.5.3",
+        release_id: 26131621
+    },
+    {
+        package: "acidanthera/OpenCorePkg",
+        version: "0.5.9",
+        release_id: 27099009
+    },
+    {
+        package: "acidanthera/VirtualSMC",
+        version: "1.1.4",
+        release_id: 27097718
+    },
+    {
+        package: "acidanthera/Lilu",
+        version: "1.4.5",
+        release_id: 27097497
+    },
+    {
+        package: "acidanthera/AirportBrcmFixup",
+        version: "2.0.7",
+        release_id: 26131640
+    },
+    {
+        package: "acidanthera/AppleALC",
+        version: "1.5.0",
+        release_id: 27097684
+    },
+    {
+        package: "acidanthera/WhateverGreen",
+        version: "1.4.0",
+        release_id: 27097811
+    },
+    {
+        package: "acidanthera/IntelMausi",
+        version: "1.0.3",
+        release_id: 27097780
+    }
+]
+
+export const allVersions: GithubPackageAllVersions[] = [
+    {
+        package: "khronokernel/SmallTree-I211-AT-patch",
+        version: [
+            {
+                release_id: "1.3.0",
+                release_version: 25575621
+            },
+            {
+                release_id: "1.2.5",
+                release_version: 24018936
+            }
+        ]
+    },
+    {
+        package: "Mieze/AtherosE2200Ethernet",
+        version: [
+            {
+                release_id: "2.2.2",
+                release_version: 14061603
+            }
+        ]
+    },
+    {
+        package: "Sniki/OS-X-USB-Inject-All",
+        version: [
+            {
+                release_id: "v0.7.5",
+                release_version: 24748286
+            },
+            {
+                release_id: "v0.7.3",
+                release_version: 19475499
+            },
+            {
+                release_id: "v0.7.2",
+                release_version: 18130539
+            }
+        ]
+    },
+    {
+        package: "Mieze/RTL8111_driver_for_OS_X",
+        version: [
+            {
+                release_id: "v2.2.2",
+                release_version: 10009742
+            },
+            {
+                release_id: "RealtekRTL8111",
+                release_version: 8382667
+            }
+        ]
+    },
+    {
+        package: "acidanthera/BrcmPatchRAM",
+        version: [
+            {
+                release_id: "2.5.3",
+                release_version: 26131621
+            },
+            {
+                release_id: "2.5.2",
+                release_version: 25234789
+            },
+            {
+                release_id: "2.5.1",
+                release_version: 22780422
+            },
+            {
+                release_id: "2.5.0",
+                release_version: 21084435
+            }
+        ]
+    },
+    {
+        package: "acidanthera/OpenCorePkg",
+        version: [
+            {
+                release_id: "0.5.9",
+                release_version: 27099009
+            },
+            {
+                release_id: "0.5.8",
+                release_version: 26131694
+            },
+            {
+                release_id: "0.5.7",
+                release_version: 25234853
+            },
+            {
+                release_id: "0.5.6",
+                release_version: 24123377
+            },
+            {
+                release_id: "0.5.5",
+                release_version: 23331292
+            },
+            {
+                release_id: "0.5.4",
+                release_version: 22780487
+            },
+            {
+                release_id: "0.5.3",
+                release_version: 21892736
+            },
+            {
+                release_id: "0.5.2",
+                release_version: 21084502
+            },
+            {
+                release_id: "0.5.1",
+                release_version: 20538481
+            },
+            {
+                release_id: "0.5.0",
+                release_version: 19827631
+            },
+            {
+                release_id: "0.0.4",
+                release_version: 19214242
+            },
+            {
+                release_id: "v0.0.3",
+                release_version: 18380238
+            },
+            {
+                release_id: "0.0.2",
+                release_version: 17570964
+            },
+            {
+                release_id: "0.0.1",
+                release_version: 17145062
+            }
+        ]
+    },
+    {
+        package: "acidanthera/VirtualSMC",
+        version: [
+            {
+                release_id: "1.1.4",
+                release_version: 27097718
+            },
+            {
+                release_id: "1.1.3",
+                release_version: 26131486
+            },
+            {
+                release_id: "1.1.2",
+                release_version: 25234812
+            },
+            {
+                release_id: "1.1.1",
+                release_version: 23331219
+            },
+            {
+                release_id: "1.1.0",
+                release_version: 22780402
+            },
+            {
+                release_id: "1.0.9",
+                release_version: 21084415
+            },
+            {
+                release_id: "1.0.8",
+                release_version: 20538424
+            },
+            {
+                release_id: "1.0.7",
+                release_version: 19214914
+            },
+            {
+                release_id: "1.0.6",
+                release_version: 18434075
+            },
+            {
+                release_id: "1.0.5",
+                release_version: 18380094
+            },
+            {
+                release_id: "1.0.4",
+                release_version: 17570329
+            },
+            {
+                release_id: "1.0.3",
+                release_version: 17142250
+            },
+            {
+                release_id: "1.0.2",
+                release_version: 14657403
+            },
+            {
+                release_id: "1.0.1",
+                release_version: 13729636
+            },
+            {
+                release_id: "1.0.0",
+                release_version: 12837606
+            }
+        ]
+    },
+    {
+        package: "acidanthera/Lilu",
+        version: [
+            {
+                release_id: "1.4.5",
+                release_version: 27097497
+            },
+            {
+                release_id: "1.4.4",
+                release_version: 26131297
+            },
+            {
+                release_id: "1.4.3",
+                release_version: 25234596
+            },
+            {
+                release_id: "1.4.2",
+                release_version: 24123072
+            },
+            {
+                release_id: "1.4.1",
+                release_version: 22780229
+            },
+            {
+                release_id: "1.4.0",
+                release_version: 21892419
+            },
+            {
+                release_id: "1.3.9",
+                release_version: 21084089
+            },
+            {
+                release_id: "1.3.8",
+                release_version: 19214835
+            },
+            {
+                release_id: "1.3.7",
+                release_version: 18379599
+            },
+            {
+                release_id: "1.3.6",
+                release_version: 17569835
+            },
+            {
+                release_id: "1.3.5",
+                release_version: 16250700
+            },
+            {
+                release_id: "1.3.4",
+                release_version: 15637100
+            },
+            {
+                release_id: "1.3.3",
+                release_version: 15367620
+            },
+            {
+                release_id: "1.3.1",
+                release_version: 14745731
+            },
+            {
+                release_id: "1.3.0",
+                release_version: 14728041
+            },
+            {
+                release_id: "1.2.9",
+                release_version: 14656708
+            },
+            {
+                release_id: "1.2.8",
+                release_version: 13728836
+            },
+            {
+                release_id: "1.2.7",
+                release_version: 12834282
+            },
+            {
+                release_id: "1.2.6",
+                release_version: 12374348
+            },
+            {
+                release_id: "1.2.5",
+                release_version: 12006713
+            },
+            {
+                release_id: "1.2.4",
+                release_version: 11728679
+            },
+            {
+                release_id: "1.2.3",
+                release_version: 10438798
+            },
+            {
+                release_id: "1.2.2",
+                release_version: 9415247
+            },
+            {
+                release_id: "1.2.1",
+                release_version: 8478093
+            },
+            {
+                release_id: "1.2.0",
+                release_version: 8061280
+            },
+            {
+                release_id: "1.1.7",
+                release_version: 7535484
+            },
+            {
+                release_id: "1.1.6",
+                release_version: 7182424
+            },
+            {
+                release_id: "1.1.5",
+                release_version: 6884518
+            },
+            {
+                release_id: "1.1.4",
+                release_version: 6618879
+            },
+            {
+                release_id: "1.1.3",
+                release_version: 6586588
+            }
+        ]
+    },
+    {
+        package: "acidanthera/AirportBrcmFixup",
+        version: [
+            {
+                release_id: "2.0.7",
+                release_version: 26131640
+            },
+            {
+                release_id: "2.0.6",
+                release_version: 23331255
+            },
+            {
+                release_id: "2.0.5",
+                release_version: 22780386
+            },
+            {
+                release_id: "2.0.4",
+                release_version: 21084327
+            },
+            {
+                release_id: "2.0.3",
+                release_version: 19214841
+            },
+            {
+                release_id: "2.0.2",
+                release_version: 18380014
+            },
+            {
+                release_id: "2.0.0",
+                release_version: 17142532
+            },
+            {
+                release_id: "1.1.9",
+                release_version: 14798704
+            },
+            {
+                release_id: "1.1.8",
+                release_version: 14728085
+            },
+            {
+                release_id: "1.1.7",
+                release_version: 14656920
+            },
+            {
+                release_id: "v1.1.6",
+                release_version: 13251048
+            },
+            {
+                release_id: "1.1.5",
+                release_version: 12374500
+            },
+            {
+                release_id: "v1.1.4",
+                release_version: 12228959
+            },
+            {
+                release_id: "v.1.1.3",
+                release_version: 11353186
+            },
+            {
+                release_id: "v1.1.2",
+                release_version: 10156768
+            },
+            {
+                release_id: "v1.1.1",
+                release_version: 10016442
+            },
+            {
+                release_id: "v.1.1.0",
+                release_version: 10003777
+            }
+        ]
+    },
+    {
+        package: "acidanthera/AppleALC",
+        version: [
+            {
+                release_id: "1.5.0",
+                release_version: 27097684
+            },
+            {
+                release_id: "1.4.9",
+                release_version: 26131406
+            },
+            {
+                release_id: "1.4.8",
+                release_version: 25234350
+            },
+            {
+                release_id: "1.4.7",
+                release_version: 24123238
+            },
+            {
+                release_id: "1.4.6",
+                release_version: 23331182
+            },
+            {
+                release_id: "1.4.5",
+                release_version: 22780375
+            },
+            {
+                release_id: "1.4.4",
+                release_version: 21892510
+            },
+            {
+                release_id: "1.4.3",
+                release_version: 21084307
+            },
+            {
+                release_id: "1.4.2",
+                release_version: 20538392
+            },
+            {
+                release_id: "1.4.1",
+                release_version: 19827507
+            },
+            {
+                release_id: "1.4.0",
+                release_version: 19214849
+            },
+            {
+                release_id: "1.3.9",
+                release_version: 18379889
+            },
+            {
+                release_id: "1.3.8",
+                release_version: 17569950
+            },
+            {
+                release_id: "1.3.7",
+                release_version: 16644505
+            },
+            {
+                release_id: "1.3.6",
+                release_version: 16250776
+            },
+            {
+                release_id: "1.3.5",
+                release_version: 15367800
+            },
+            {
+                release_id: "1.3.4",
+                release_version: 14656776
+            },
+            {
+                release_id: "1.3.3",
+                release_version: 13728946
+            },
+            {
+                release_id: "1.3.2",
+                release_version: 12834581
+            },
+            {
+                release_id: "1.3.1",
+                release_version: 12374430
+            },
+            {
+                release_id: "1.3.0",
+                release_version: 12006860
+            },
+            {
+                release_id: "1.2.8",
+                release_version: 11793303
+            },
+            {
+                release_id: "1.2.7",
+                release_version: 11292278
+            },
+            {
+                release_id: "1.2.6",
+                release_version: 10446835
+            },
+            {
+                release_id: "1.2.4",
+                release_version: 10329929
+            },
+            {
+                release_id: "1.2.3",
+                release_version: 10015971
+            },
+            {
+                release_id: "1.2.2",
+                release_version: 9047681
+            },
+            {
+                release_id: "1.2.1",
+                release_version: 8478279
+            },
+            {
+                release_id: "1.2.0",
+                release_version: 8061295
+            },
+            {
+                release_id: "1.1.4",
+                release_version: 7535487
+            }
+        ]
+    },
+    {
+        package: "acidanthera/WhateverGreen",
+        version: [
+            {
+                release_id: "1.4.0",
+                release_version: 27097811
+            },
+            {
+                release_id: "1.3.9",
+                release_version: 26131429
+            },
+            {
+                release_id: "1.3.8",
+                release_version: 25234575
+            },
+            {
+                release_id: "1.3.7",
+                release_version: 24123270
+            },
+            {
+                release_id: "1.3.6",
+                release_version: 22780329
+            },
+            {
+                release_id: "1.3.5",
+                release_version: 21892539
+            },
+            {
+                release_id: "1.3.4",
+                release_version: 21084404
+            },
+            {
+                release_id: "1.3.3",
+                release_version: 20538403
+            },
+            {
+                release_id: "1.3.2",
+                release_version: 19827571
+            },
+            {
+                release_id: "1.3.1",
+                release_version: 19214908
+            },
+            {
+                release_id: "1.3.0",
+                release_version: 18380082
+            },
+            {
+                release_id: "1.2.9",
+                release_version: 17570233
+            },
+            {
+                release_id: "1.2.8",
+                release_version: 16657442
+            },
+            {
+                release_id: "1.2.7",
+                release_version: 16250805
+            },
+            {
+                release_id: "1.2.6",
+                release_version: 14728107
+            },
+            {
+                release_id: "1.2.5",
+                release_version: 14656867
+            },
+            {
+                release_id: "1.2.4",
+                release_version: 13728962
+            },
+            {
+                release_id: "1.2.3",
+                release_version: 13029268
+            },
+            {
+                release_id: "1.2.2",
+                release_version: 12834373
+            },
+            {
+                release_id: "1.2.1",
+                release_version: 12374514
+            },
+            {
+                release_id: "1.2.0",
+                release_version: 12006997
+            },
+            {
+                release_id: "1.1.7",
+                release_version: 10329998
+            },
+            {
+                release_id: "1.1.6",
+                release_version: 10016029
+            },
+            {
+                release_id: "ac8b935",
+                release_version: 9634211
+            },
+            {
+                release_id: "1.1.4",
+                release_version: 8983740
+            },
+            {
+                release_id: "1.1.3",
+                release_version: 8478270
+            },
+            {
+                release_id: "1.1.2",
+                release_version: 8211568
+            },
+            {
+                release_id: "1.1.1",
+                release_version: 8169727
+            },
+            {
+                release_id: "1.1.0",
+                release_version: 8061330
+            },
+            {
+                release_id: "1.0.4",
+                release_version: 7535493
+            }
+        ]
+    },
+    {
+        package: "acidanthera/IntelMausi",
+        version: [
+            {
+                release_id: "1.0.3",
+                release_version: 27097780
+            },
+            {
+                release_id: "1.0.2",
+                release_version: 19827545
+            },
+            {
+                release_id: "1.0.1",
+                release_version: 19214141
+            },
+            {
+                release_id: "1.0.0",
+                release_version: 17570486
+            }
+        ]
+    }
+]
