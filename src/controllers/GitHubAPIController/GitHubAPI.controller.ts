f1abc561b8e181a2062417279480dfab64d944e4
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2020-06-14 13:06:00 +0530

    Added additional functionality for bitbucket/discord downloads

--- src/controllers/GitHubAPIController/GitHubAPI.controller.ts
+++ src/controllers/GitHubAPIController/GitHubAPI.controller.ts
@@ -2,9 +2,10 @@ import * as express from 'express'
 import Axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios'
 import { Request, Response, NextFunction } from 'express'
 import IControllerBase from '../../interfaces/IControllerBase.interface'
-import { packagesList } from '../../packages/index'
+import { packagesList, packagesListType } from '../../packages/index'
 import { GITHUB_TOKEN } from '../../utils/config'
 import { GithubPackageLatest, GithubPackageAllVersions } from '../../types'
+import { getFullDateFunction } from '../../utils'
 // import { latest, allVersions } from '../../../dummyData'
 
 class GitHubAPIController implements IControllerBase {
@@ -29,24 +30,58 @@ class GitHubAPIController implements IControllerBase {
     }
 
     private getAllVersions() {
-        packagesList.forEach((pkg: string) => {
+        packagesList.forEach((pkg: packagesListType) => {
             // const pkg = "acidanthera/OpenCorePkg"
-            const [user, repo] = pkg.split('/')
-            const { url, config } = this.GitHubAPIURL(user, repo, false)
-            Axios.get(url, config)
-                .then((res: AxiosResponse) => {
-                    if (res.data[0] !== undefined) {
-                        this._packagesLatest.push({ package: pkg, version: res.data[0].tag_name, release_id: res.data[0].id })
-                    }
-                    this._packagesAllVersions.push({ package: pkg, version: res.data.map((x: any) => ({ release_id: x.id, release_version: x.tag_name })) })
-                })
-                .catch((err: AxiosError) => console.log(err))
+            const { name, endpoint } = pkg
+            const [user, repo] = name.split('/')
 
+            switch (endpoint) {
+                case 'discord':
+                    this._packagesLatest.push({ package: pkg.name, version: 0, release_id: 0, downloadLink: pkg.download })
+                    this._packagesAllVersions.push({ package: pkg.name, version: [{ release_id: 0, release_version: 0, downloadLink: pkg.download }] })
+                    return;
+                case 'github':
+                    const { giturl, config } = this.GitHubAPIURL(user, repo, false)
+                    return Axios
+                        .get(giturl, config)
+                        .then((res: AxiosResponse) => {
+                            if (res.data[0] !== undefined) {
+                                this._packagesLatest.push({ package: pkg.name, version: res.data[0].tag_name, release_id: res.data[0].id })
+                            }
+                            this._packagesAllVersions.push({ package: pkg.name, version: res.data.map((x: any) => ({ release_id: x.id, release_version: x.tag_name })) })
+                        })
+                        .catch((err: AxiosError) => console.log(err))
+
+                case 'bitbucket':
+                    const { bitbucketurl } = this.BitbucketAPIURL(user, repo)
+                    return Axios
+                        .get(bitbucketurl)
+                        .then((res: AxiosResponse) => {
+                            if (res.data.values[0] !== undefined) {
+                                const { date, month, year } = getFullDateFunction(res.data.values[0].created_on)
+                                this._packagesLatest.push({ package: pkg.name, version: `${year}-${month + 1}${date}`, release_id: 0, downloadLink: res.data.values[0].links.self.href })
+                            }
+                            this._packagesAllVersions.push({
+                                package: pkg.name, version: res.data.values.map((x: any) => {
+                                    const { date, month, year } = getFullDateFunction(x.created_on)
+                                    return { release_id: 0, release_version: `${year}-${month}${date}`, downloadLink: x.links.self.href }
+                                })
+                            })
+                        })
+                        .catch((err: AxiosError) => console.log(err))
+
+                default:
+                    return
+            }
         })
     }
 
-    private GitHubAPIURL = (user: string, repo: string, isLatest: boolean, version?: string | number | undefined): { url: string, config: AxiosRequestConfig } => {
-        return { url: `https://api.github.com/repos/${user}/${repo}/releases${version ? `/${version}` : ""}${isLatest ? `/latest` : ""}`, config: { headers: { Authorization: `Token ${GITHUB_TOKEN as string}` } } }
+    private GitHubAPIURL = (user: string, repo: string, isLatest: boolean, version?: string | number | undefined): { giturl: string, config: AxiosRequestConfig } => {
+        return { giturl: `https://api.github.com/repos/${user}/${repo}/releases${version ? `/${version}` : ""}${isLatest ? `/latest` : ""}`, config: { headers: { Authorization: `Token ${GITHUB_TOKEN as string}` } } }
+    }
+
+    private BitbucketAPIURL = (user: string, repo: string): { bitbucketurl: string, config?: AxiosRequestConfig } => {
+        return { bitbucketurl: `https://api.bitbucket.org/2.0/repositories/${user}/${repo}/downloads` }
     }
 
     private dummyGet = (req: Request, res: Response) => {
@@ -65,7 +100,6 @@ class GitHubAPIController implements IControllerBase {
             res.send(400).send({ status: 400, message: "Bad Request, missing Parameters" })
         } else {
             if (version !== undefined) {
-                // Need to change code from and replace release_id with release_version due to an earlier mistake.
                 const specificVersion = this._packagesAllVersions.map(pkg => {
                     if (pkg.package === `${user}/${repo}`) {
                         const foundVersion = pkg.version.find(pkgV => pkgV.release_version == version)
@@ -74,17 +108,17 @@ class GitHubAPIController implements IControllerBase {
                     return null
                 }).filter(Boolean)
 
-                const { url, config } = this.GitHubAPIURL(user, repo, false, specificVersion[0]?.release_id)
+                const { giturl, config } = this.GitHubAPIURL(user, repo, false, specificVersion[0]?.release_id)
 
-                Axios.get(url, config)
+                Axios.get(giturl, config)
                     .then((apiData: AxiosResponse) => {
                         res.status(200).send({ version: specificVersion, message: "SPECIFIC VERSION RELEASE", data: apiData.data })
                     })
                     .catch((err: AxiosError) => res.status(400).send({ error: err, message: "Error" }))
             } else {
                 const latestVersion = this._packagesLatest.find(pkg => pkg.package === `${user}/${repo}` && pkg.version)
-                const { url, config } = this.GitHubAPIURL(user, repo, true)
-                Axios.get(url, config)
+                const { giturl, config } = this.GitHubAPIURL(user, repo, true)
+                Axios.get(giturl, config)
                     .then((apiData: AxiosResponse) => {
                         res.status(200).send({ version: latestVersion, message: "LATEST RELEASE", data: apiData.data })
                     })
